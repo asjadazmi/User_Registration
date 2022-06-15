e9f2329ac5db72af719cc05fb483b1417e193210
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-06-15 23:27:37 +0530

    Added : Validation of Email

--- src/com/bridgeuserregistration/UserRegistration.java
+++ src/com/bridgeuserregistration/UserRegistration.java
@@ -5,6 +5,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
+//	FirstName
 	public static void firstNameValidation(String name) {
 			String firstNameRegex="^[A-Z]{1}[a-z]{2}$";
 			Pattern pattern=Pattern.compile(firstNameRegex);
@@ -17,6 +18,7 @@ public class UserRegistration {
 			}
 			
 		}
+//	LastName
 	public static void lastNameValidation(String name) {
 		String lastNameRegex="^[A-Z]{1}[a-z]{2}$";
 		Pattern pattern=Pattern.compile(lastNameRegex);
@@ -29,18 +31,33 @@ public class UserRegistration {
 		}
 		
 	}
+//	Email validation
+	public static void emailValidation(String email) {
+		String emailRegex="^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$";
+		Pattern pattern=Pattern.compile(emailRegex);
+		Matcher matcher=pattern.matcher(email);
+		if(matcher.find()) {
+			System.out.println("its valid");
+		}
+		else {
+			System.out.println("not valid");
+		}
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to the user registration !");
 		UserRegistration v = new UserRegistration();
-		Scanner sc=new Scanner(System.in);
+	Scanner sc=new Scanner(System.in);
 		System.out.println("Enter the first name");
 		String firstName = sc.next();
 		v.firstNameValidation(firstName);
 		System.out.println("Enter the last name");
 		String lastName = sc.next();
 	    v.lastNameValidation(lastName);
+	    System.out.println("Enter your email");
+		String email = sc.next();
+	    v.emailValidation(email);
 	    
-    
-
 	}
-}
+	
+	}
+	
