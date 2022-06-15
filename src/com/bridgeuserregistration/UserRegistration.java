253aa3c9afaa5712784f89510f9574faf25fe3d1
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-06-15 17:56:10 +0530

    Added : Welcome to the User Registration problem

---
+++ src/com/bridgeuserregistration/UserRegistration.java
@@ -0,0 +1,7 @@
+package com.bridgeuserregistration;
+
+public class UserRegistration {
+	public static void main(String[] args) {
+         System.out.println("Welcome to the user registration !");
+    }
+}
