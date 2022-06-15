a37bb3f0c110c50963765caa2c6ae74cd50b889f
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-06-15 18:22:39 +0530

    Added : Validation of the First And Last Name

--- src/com/bridgeuserregistration/UserRegistration.java
+++ src/com/bridgeuserregistration/UserRegistration.java
@@ -5,9 +5,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-	public static void nameValidation(String name) {
-			String nameregex="^[A-Z]{1}[a-z]{2}$";
-			Pattern pattern=Pattern.compile(nameregex);
+	public static void firstNameValidation(String name) {
+			String firstNameRegex="^[A-Z]{1}[a-z]{2}$";
+			Pattern pattern=Pattern.compile(firstNameRegex);
 			Matcher matcher=pattern.matcher(name);
 			if(matcher.find()) {
 				System.out.println("its valid");
@@ -17,13 +17,29 @@ public class UserRegistration {
 			}
 			
 		}
+	public static void lastNameValidation(String name) {
+		String lastNameRegex="^[A-Z]{1}[a-z]{2}$";
+		Pattern pattern=Pattern.compile(lastNameRegex);
+		Matcher matcher=pattern.matcher(name);
+		if(matcher.find()) {
+			System.out.println("its valid");
+		}
+		else {
+			System.out.println("its not valid");
+		}
+		
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to the user registration !");
 		UserRegistration v = new UserRegistration();
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter the first name");
 		String firstName = sc.next();
-	    v.nameValidation(firstName);
+		v.firstNameValidation(firstName);
+		System.out.println("Enter the last name");
+		String lastName = sc.next();
+	    v.lastNameValidation(lastName);
+	    
     
 
 	}
