621d34454af9ab37578657eb61b0179d9b565738
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-06-25 10:50:03 +0530

    Added :Should have Atleast one Upper Case

--- src/com/bridgeuserregistration/UserRegistration.java
+++ src/com/bridgeuserregistration/UserRegistration.java
@@ -5,9 +5,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
+
 //	FirstName
 	public static void firstNameValidation(String name) {
-			String firstNameRegex="^[A-Z]{1}[a-z]{2}$";
+			String firstNameRegex="^[A-Z]{1}[a-z]{2,}$";
 			Pattern pattern=Pattern.compile(firstNameRegex);
 			Matcher matcher=pattern.matcher(name);
 			if(matcher.find()) {
@@ -19,7 +20,7 @@ public class UserRegistration {
 		}
 //	LastName
 	public static void lastNameValidation(String name) {
-		String lastNameRegex="^[A-Z]{1}[a-z]{2}$";
+		String lastNameRegex="^[A-Z]{1}[a-z]{2,}$";
 		Pattern pattern=Pattern.compile(lastNameRegex);
 		Matcher matcher=pattern.matcher(name);
 		if(matcher.find()) {
@@ -30,9 +31,10 @@ public class UserRegistration {
 		}
 		
 	}
+//	abc.xyz@bl.co.in
 //	Email validation
 	public static void emailValidation(String email) {
-		String emailRegex="^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$";
+		String emailRegex="^[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?$";
 		Pattern pattern=Pattern.compile(emailRegex);
 		Matcher matcher=pattern.matcher(email);
 		if(matcher.find()) {
@@ -42,6 +44,7 @@ public class UserRegistration {
 			System.out.println("not valid");
 		}
 	}
+ 
 //	Mobile Format
 	public static void mobileValidation(String mobile) {
 		String mobileRegex="^(91|0)?[7-9]{1}+[0-9]{9}$";
@@ -58,6 +61,18 @@ public class UserRegistration {
 	public static void passwordValidationRule1(String password) {
 		String passwordRegex="^([A-Za-z0-9]*[//!//@//#//$//%//^//&//*//(//)//_//-//+//=//{//}//[//]//?//>//.//<//,//]*){8}$";
 		Pattern pattern=Pattern.compile(passwordRegex);
+		Matcher matcher=pattern.matcher(password);
+		if(matcher.find()) {
+			System.out.println("its valid");
+		}
+		else {
+			System.out.println("not valid");
+		}
+}
+//	password rule 2
+	public static void passwordValidationRule2(String password) {
+		String regexPassword="[A-Za-z0-9]{8}$";
+		Pattern pattern=Pattern.compile(regexPassword);
 		Matcher matcher=pattern.matcher(password);
 		if(matcher.find()) {
 			System.out.println("its valid");
@@ -86,7 +101,10 @@ public class UserRegistration {
 	    System.out.println("Enter your password number");
 		String password = sc.next();
 	    v.passwordValidationRule1(password);
-	}
+	    System.out.println("Enter your password number");
+		String password2 = sc.next();
+	    v.passwordValidationRule2(password2);
+	 
 	
 	}
 	
